93336013534dadab5a798c6bce7a9b711b2d893f
larutow <lucas.rutowski@gmail.com>
2020-11-13 12:15:00 -0600

    added Customer model properties

--- WasteManager/WasteManager/Models/Customer.cs
+++ WasteManager/WasteManager/Models/Customer.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WasteManager.Models
 {
-    public class Class
+    public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string CityState { get; set; }
+        public int zip { get; set; }
+        public DayOfWeek WeeklyPickupDay { get; set; }
+        public DateTime MonthlyExtraDate { get; set; }
+        public DateTime PickupPause { get; set; }
+        public DateTime PickupResume { get; set; }
+        public double Balance { get; set; }
+
+        //consider parameters for javascript maps api
+
+        //payment information
+
+
+
+
     }
 }
